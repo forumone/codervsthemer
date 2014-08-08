ebfa872f208389ee5dd885c5abc40006aba85d8f
Campbell Vertesi <campbell@vertesi.com>
2014-08-08 11:55:14 +0200

    adding drush aliases file

---
+++ sites/all/drush/cvst.aliases.drushrc.php
@@ -0,0 +1,11 @@
+<?php
+// Site cvst, environment master
+$aliases['master'] = array(
+  'parent' => '@parent',
+  'site' => 'cvst',
+  'env' => 'master',
+  'root' => '/var/www/codervsthemer.forumone.com/public_html/codervsthemer/master',
+  'uri' => 'http://codervsthemer.forumone.com/master',
+  'remote-host' => 'codervsthemer.forumone.com',
+  'remote-user' => 'cvst',
+);
