900117a7164d0663d3f910ae5c8600dcd6059eac
Campbell Vertesi <campbell@vertesi.com>
2014-10-01 16:43:24 +0200

    ninja scripts

---
+++ .ninja.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+cat ./.ninja-ascii.txt
+echo " "
+echo " Ninja download script activated!"
+echo "Downloading Coder vs Themer modules: ds, panels, panels_extra_layouts"
+/usr/bin/drush en -y ds panels panels_extra_layouts
+
+echo "Generating random insult..."
+echo '
+
+
+
+
+
+
+'
+echo "Adam smells bad."
