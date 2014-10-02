20066b40907e425f0f06b0734bf5ef6e198b87eb
Campbell Vertesi <campbell@vertesi.com>
2014-10-02 03:50:53 +0200

    updating ninja.sh

--- .ninja.sh
+++ .ninja.sh
@@ -4,6 +4,11 @@ echo " "
 echo " Ninja download script activated!"
 echo "Downloading Coder vs Themer modules: ds, panels, panels_extra_layouts"
 /usr/bin/drush en -y ds panels panels_extra_layouts
+echo "."
+echo "."
+echo "."
+echo "Enabling Coder vs Themer modules"
+/usr/bin/drush en -y page_manager views_content ds_extras ds_format ds_forms ds_ui panels_mini 
 
 echo "Generating random insult..."
 echo '
